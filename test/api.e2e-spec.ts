9591b600e9dee96c99438eeeb9696a90bfeeb659
Haralan Dobrev <harry@clippings.com>
2020-11-18 11:54:55 +0200

    Add initial configuration

---
+++ test/api.e2e-spec.ts
@@ -0,0 +1,24 @@
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { ApiModule } from '../src/api.module';
+import { INestApplication } from '@nestjs/common';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [ApiModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
