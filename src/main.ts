bd72793e1da98c9236a40a221ad3f6ec29d535ea
Haralan Dobrev <harry@clippings.com>
2020-12-07 20:45:48 +0200

    Enable cross-origin requests for web clients

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { setUpSwagger, NotFoundExceptionFilter } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   setUpSwagger(app);
   app.useGlobalFilters(new NotFoundExceptionFilter());
   await app.listen(app.get(ConfigService).get<number>('PORT', 3000));
