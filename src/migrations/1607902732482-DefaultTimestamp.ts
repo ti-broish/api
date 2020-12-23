879c8feeb862f7486d00649740b748be8ccff1e9
Haralan Dobrev <harry@clippings.com>
2020-12-23 15:58:26 +0200

    Upgrade protocols database structure

---
+++ src/migrations/1607902732482-DefaultTimestamp.ts
@@ -0,0 +1,17 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class DefaultTimestamp1607902732482 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`
+        alter table "protocol_actions" alter column timestamp set default CURRENT_TIMESTAMP;
+      `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`
+        alter table "protocol_actions" alter column timestamp set default null;
+      `);
+    }
+
+}
