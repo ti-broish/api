b9800e6ac9b3d792188f2d61edfdc3a4dc63712b
Haralan Dobrev <harry@clippings.com>
2020-12-26 00:56:32 +0200

    Rename protocol-actions entity file accordingly to entity name

--- src/protocols/entities/protocol.entity.ts
+++ src/protocols/entities/protocol.entity.ts
@@ -1,7 +1,7 @@
 import { Section } from '../../sections/entities';
 import { Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryColumn } from "typeorm";
 import { ulid } from 'ulid';
-import { ProtocolAction, ProtocolActionType } from './protocol-actions.entity';
+import { ProtocolAction, ProtocolActionType } from './protocol-action.entity';
 import { ProtocolData } from './protocol-data.entity';
 import { Picture } from 'src/pictures/entities/picture.entity';
 import { ProtocolResult } from './protocol-result.entity';
