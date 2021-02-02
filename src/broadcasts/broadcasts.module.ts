dfc53f8449bdb1cdce94946bf071c1ac7bfa2914
Haralan Dobrev <harry@clippings.com>
2021-02-03 00:23:01 +0200

    Extract posts from the broadcasts module

--- src/broadcasts/broadcasts.module.ts
+++ src/broadcasts/broadcasts.module.ts
@@ -5,10 +5,12 @@ import { Broadcast } from '../broadcasts/entities/broadcast.entity';
 import { BroadcastsController } from './api/broadcasts.controller';
 import { BroadcastsRepository } from '../broadcasts/entities/broadcasts.repository';
 import { BroadcastsPublishTask } from './notifications/broadcasts-publish.task';
+import { CaslModule } from 'src/casl/casl.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Broadcast]),
+    CaslModule,
     UsersModule,
   ],
   providers: [BroadcastsRepository, BroadcastsPublishTask],
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Get, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Ability } from '@casl/ability';
+import { Body, Controller, Get, HttpCode, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { InjectUser } from 'src/auth/decorators';
+import { Action } from 'src/casl/action.enum';
+import { CheckPolicies } from 'src/casl/check-policies.decorator';
+import { PoliciesGuard } from 'src/casl/policies.guard';
 import { User } from 'src/users/entities';
-import { PostsRepository } from '../../posts/entities/posts.repository';
+import { PostsRepository } from './entities/posts.repository';
 import { PostDto } from './post.dto';
 
 @Controller('posts')
@@ -10,12 +14,16 @@ export class PostsController {
 
   @Get()
   @HttpCode(200)
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: Ability) => ability.can(Action.Read, Post))
   async index(): Promise<PostDto[]> {
     return (await this.repo.findAllPublishedAndListed()).map(PostDto.fromEntity);
   }
 
   @Post()
   @HttpCode(201)
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: Ability) => ability.can(Action.Create, Post))
   @UsePipes(new ValidationPipe({ transform: true, transformOptions: { groups: ['create'] }, groups: ['create'] }))
   async create(
     @Body() postDto: PostDto,
