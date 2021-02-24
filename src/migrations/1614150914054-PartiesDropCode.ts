20337fb10a4ee6ecc2b4a0d6c2fc894520abf2b3
Haralan Dobrev <harry@clippings.com>
2021-02-24 09:21:20 +0200

    Remove unused parties.code

---
+++ src/migrations/1614150914054-PartiesDropCode.ts
@@ -0,0 +1,24 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class PartiesDropCode1614150914054 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`
+        alter table "parties" drop column "code";
+      `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`
+        alter table "parties"
+        add column "code" int
+      `);
+      await queryRunner.query(`
+        update "parties" set "code" = "id";
+      `);
+      await queryRunner.query(`
+        alter table "parties" alter column "code" set not null;
+      `);
+    }
+
+}
