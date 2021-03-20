d3974771af4ec5f82feb1d8428b650ea27160e61
Haralan Dobrev <harry@clippings.com>
2021-03-20 12:56:52 +0200

    Add script to send email verification to users

---
+++ scripts/send-email-verification.js
@@ -0,0 +1,34 @@
+const admin = require('firebase-admin')
+const firebase = require('firebase')
+const serviceAccount = require("../firebase.json");
+
+require('dotenv').config();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+});
+
+const adminAuth = admin.auth();
+const auth = firebase.auth();
+
+const emailUser = async (email) => {
+  try {
+    const userRecord = await adminAuth.getUserByEmail(email);
+    const token = await adminAuth.createCustomToken(userRecord.uid)
+    await auth.signInWithCustomToken(token)
+    const currentUser = auth.currentUser;
+    await currentUser.sendEmailVerification();
+    console.log('email sent to', email);
+    await auth.signOut();
+    process.exit(0);
+  } catch (error) {
+    console.log('Error: ', error);
+    process.exit(1);
+  }
+};
+
+emailUser(process.argv[2]);
