878136030509b15c96aabe15290663e990d67ed1
Haralan Dobrev <harry@clippings.com>
2021-03-20 12:51:11 +0200

    Create a script to list all users and their email verification

---
+++ scripts/list-user-verification.js
@@ -0,0 +1,51 @@
+const admin = require('firebase-admin')
+const serviceAccount = require("../firebase.json");
+
+require('dotenv').config()
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ti-broish.firebaseio.com",
+});
+let hasErrors = false;
+
+const listAllUsers = async () => {
+  return new Promise(async (resolve, reject) => {
+
+    // List batch of users, 1000 at a time.
+    const listBatchOfUsers = async (nextPageToken) => {
+      await admin
+      .auth()
+      .listUsers(1000, nextPageToken)
+      .then(async (listUsersResult) => {
+        listUsersResult.users.forEach((userRecord) => {
+          const { email, emailVerified } = userRecord.toJSON();
+          console.log(email, emailVerified);
+        });
+        if (listUsersResult.pageToken) {
+          // List next batch of users.
+          await listBatchOfUsers(listUsersResult.pageToken);
+        } else {
+          resolve();
+        }
+      })
+      .catch((error) => {
+        console.log('Error listing users:', error);
+        hasErrors = true;
+      });
+    };
+    await listBatchOfUsers();
+  });
+};
+
+const runScript = async () => {
+  try {
+    // Start listing users from the beginning, 1000 at a time.
+    await listAllUsers();
+    process.exit(hasErrors ? 1 : 0)
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runScript();
