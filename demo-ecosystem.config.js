6527750b4baa0b33f1dd4189fe64afe045b5fc47
Haralan Dobrev <harry@clippings.com>
2021-03-31 10:37:01 +0300

    Add demo deployment pm2 ecosystem

---
+++ demo-ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: "ti-broish-demo",
+    script: 'dist/main.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    instances: 1,
+    error_file: 'err.log',
+    out_file: 'out.log',
+    log_file: 'combined.log',
+  }],
+
+  deploy : {
+    production : {
+      user : 'hkdobrev',
+      host : ['ti-broish-api'],
+      ref  : 'origin/main',
+      repo : 'git@github.com:Da-Bulgaria/ti-broish-api.git',
+      path : '/var/www/ti-broish-demo',
+      'post-setup': 'ls -la',
+      // 'pre-deploy-local': 'npm run build',
+      'post-deploy' : 'npm ci --omit=dev && npm run build && cp $PWD/../shared/.env $PWD/../shared/firebase.json $PWD/ && npm run migration:run && pm2 reload demo-ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
