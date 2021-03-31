c5b448792060e24045468e34a5f25d4abb4c9891
Haralan Dobrev <harry@clippings.com>
2021-03-31 08:44:57 +0300

    Graceful shutdown when reloading the server

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,20 @@ import { AppModule } from './app.module';
 import { setUpSwagger, jsonMiddleware } from './config';
 import { TranslateStatusInterceptor } from './i18n/translate-status.interceptor';
 import { I18nService } from 'nestjs-i18n';
+import { ShutdownSignal } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  if (app.get(ConfigService).get<boolean>('API_DOCS', false)) {
+  const config = app.get(ConfigService);
+  if (config.get<boolean>('API_DOCS', false)) {
     setUpSwagger(app);
   }
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalInterceptors(new TranslateStatusInterceptor(app.get(I18nService)));
   app.use(jsonMiddleware)
-  await app.listen(app.get(ConfigService).get<number>('PORT', 3000));
+  if (config.get('NODE_ENV') === 'production') {
+    app.enableShutdownHooks([ShutdownSignal.SIGINT, ShutdownSignal.SIGTERM]);
+  }
+  await app.listen(config.get<number>('PORT', 3000));
 }
 bootstrap();
