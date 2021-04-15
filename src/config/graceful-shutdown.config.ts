5d20e77e792adfeb9b2f86916d875c0942aec4a2
Haralan Dobrev <harry@hkdobrev.com>
2021-04-15 19:41:52 +0300

    Extract all configuration from main.ts to dedicated files

---
+++ src/config/graceful-shutdown.config.ts
@@ -0,0 +1,5 @@
+import { INestApplication, ShutdownSignal } from "@nestjs/common";
+
+export function enableGracefulShutfown(app: INestApplication) {
+  app.enableShutdownHooks([ShutdownSignal.SIGINT, ShutdownSignal.SIGTERM]);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
-import { setUpSwagger, setBodySize } from './config';
-import { ShutdownSignal } from '@nestjs/common';
+import { setUpSwagger, setBodySize, enableGracefulShutfown, useContainerForValidator } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,11 +9,9 @@ async function bootstrap() {
   if (config.get<boolean>('API_DOCS', false)) {
     setUpSwagger(app);
   }
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  useContainerForValidator(app.select(AppModule));
   setBodySize(app);
-  if (config.get('NODE_ENV') === 'production') {
-    app.enableShutdownHooks([ShutdownSignal.SIGINT, ShutdownSignal.SIGTERM]);
-  }
+  enableGracefulShutfown(app);
   await app.listen(config.get<number>('PORT', 3000));
 }
 bootstrap();
