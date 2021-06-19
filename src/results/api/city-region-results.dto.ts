3ca2d5086c24e10e00c76591569de0882093b2cd
Haralan Dobrev <harry@hkdobrev.com>
2021-06-19 10:59:20 +0300

    Fix address dto import in results

--- src/results/api/city-region-results.dto.ts
+++ src/results/api/city-region-results.dto.ts
@@ -6,6 +6,7 @@ import {
   TransformOptions,
 } from 'class-transformer';
 import { CityRegion, Town } from 'src/sections/entities';
+import { AddressDto } from './address.dto';
 import { BreadcrumbDto } from './breadcrumb.dto';
 import { SectionResultsDto } from './section-results.dto';
 import { StatsDto } from './stats.dto';
