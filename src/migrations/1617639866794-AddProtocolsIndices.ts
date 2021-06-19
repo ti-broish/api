6d0a8ff0907df61752bda4e4f22a2763c53bfbea
victor.minchev@audienceplatform.tv <victor.minchev@audienceplatform.tv>
2021-06-19 18:03:55 +0300

    fix on duplicate migration for protocols_section_id_status_key constraint

--- src/migrations/1617639866794-AddProtocolsIndices.ts
+++ src/migrations/1617639866794-AddProtocolsIndices.ts
@@ -3,16 +3,12 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddProtocolsIndices1617639866794 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        create index "protocols_section_id_status_key" on "protocols" ("section_id", "status");
-        create index "protocols_status_key" on "protocols" ("status");
         create index "protocols_origin_key" on "protocols" ("origin");
       `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        drop index "protocols_section_id_status_key";
-        drop index "protocols_status_key";
         drop index "protocols_origin_key";
       `);
   }
