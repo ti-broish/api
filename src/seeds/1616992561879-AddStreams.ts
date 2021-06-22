e2298394fcd9b4b88f4bfad12f7ab76ed1da34f1
Haralan Dobrev <harry@hkdobrev.com>
2021-06-22 22:35:10 +0300

    Warn instead of erroring out and exiting for missing streams CSV seed data

--- src/seeds/1616992561879-AddStreams.ts
+++ src/seeds/1616992561879-AddStreams.ts
@@ -1,18 +1,23 @@
-import { csvToSql } from 'src/utils/csvToSql';
+import * as fs from 'fs';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import { ulid } from 'ulid';
+import { csvToSql } from 'src/utils/csvToSql';
 
 export class AddStreams1616992561879 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    (
-      await csvToSql(__dirname + '/parl-2021-04-04/streams.csv', 'streams', {
-        emptyColumnCallback: () => {
-          return ulid();
-        },
-      })
-    )
-      .split(';')
-      .map(async (sql) => await queryRunner.query(sql));
+    const streamsInput = __dirname + '/parl-2021-04-04/streams.csv';
+    if (!fs.existsSync(streamsInput)) {
+      console.warn(`Missing streams seed input: ${streamsInput}`);
+      return;
+    }
+
+    const sql = await csvToSql(streamsInput, 'streams', {
+      emptyColumnCallback: () => {
+        return ulid();
+      },
+    });
+
+    sql.split(';').forEach(async (sql) => await queryRunner.query(sql));
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
