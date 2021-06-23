56832833225ac1aa043db3a38c82565f8236969c
Haralan Dobrev <harry@hkdobrev.com>
2021-06-23 07:50:42 +0300

    Add missing indices for filtering towns

---
+++ src/migrations/1624337501383-AddTownsCountryMunicipalityIndices.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddTownsCountryMunicipalityIndices1624337501383
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        create index "towns_country_id_key" on "towns" ("country_id");
+        create index "towns_municipality_id_key" on "towns" ("country_id");
+      `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        drop index "towns_municipality_id_key";
+        drop index "towns_country_id_key";
+      `);
+  }
+}
