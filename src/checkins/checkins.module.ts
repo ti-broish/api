2dc183b6dc7fe3bd4eaa8b0be820aa040d281cb7
Haralan Dobrev <harry@hkdobrev.com>
2021-07-01 02:11:56 +0300

    Persist latest user section after a checkin in their profile

--- src/checkins/checkins.module.ts
+++ src/checkins/checkins.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CaslModule } from 'src/casl/casl.module';
+import { User } from 'src/users/entities';
+import { UsersModule } from 'src/users/users.module';
 import { Checkin } from './entities/checkin.entity';
 import { CheckinsRepository } from './entities/checkins.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Checkin]), CaslModule],
+  imports: [TypeOrmModule.forFeature([Checkin]), CaslModule, UsersModule],
   providers: [CheckinsRepository],
   exports: [CheckinsRepository],
   controllers: [],
--- src/checkins/entities/checkins.repository.ts
+++ src/checkins/entities/checkins.repository.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities';
+import { UsersRepository } from 'src/users/entities/users.repository';
 import { Repository } from 'typeorm';
 import { Checkin } from './checkin.entity';
 
 @Injectable()
 export class CheckinsRepository {
-  constructor(@InjectRepository(Checkin) private repo: Repository<Checkin>) {}
+  constructor(
+    @InjectRepository(Checkin) private checkinsRepo: Repository<Checkin>,
+    private usersRepo: UsersRepository,
+  ) {}
 
   async save(checkin: Checkin): Promise<Checkin> {
-    return await this.repo.save(checkin);
+    const savedCheckin = await this.checkinsRepo.save(checkin);
+    savedCheckin.actor.section = checkin.section;
+    await this.usersRepo.save(savedCheckin.actor);
+
+    return savedCheckin;
   }
 }
