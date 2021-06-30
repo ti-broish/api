abf2b18910dbd402d0839948c89936333b8c8374
Haralan Dobrev <harry@hkdobrev.com>
2021-06-30 23:18:38 +0300

    Add more validation on admin users filters

--- src/users/api/users-filters.dto.ts
+++ src/users/api/users-filters.dto.ts
@@ -1,24 +1,41 @@
 import { Type } from 'class-transformer';
-import { IsInt, IsOptional } from 'class-validator';
+import {
+  IsIn,
+  IsInt,
+  IsOptional,
+  IsString,
+  Length,
+  Min,
+} from 'class-validator';
+import { Role } from 'src/casl/role.enum';
 import { IsOrganizationExists } from 'src/users/api/organization-exists.constraint';
 import { PageDTO } from 'src/utils/page.dto';
 
 export class UsersFilters extends PageDTO {
   @IsOptional()
+  @IsString()
+  @Length(1, 255)
   firstName: string;
 
   @IsOptional()
+  @IsString()
+  @Length(1, 255)
   lastName: string;
 
   @IsOptional()
+  @IsString()
+  @Length(1, 255)
   email: string;
 
   @IsOptional()
   @IsInt()
   @Type(() => Number)
+  @Min(1)
   @IsOrganizationExists()
   organization: number;
 
   @IsOptional()
+  @IsString()
+  @IsIn(Object.values(Role))
   role: string;
 }
