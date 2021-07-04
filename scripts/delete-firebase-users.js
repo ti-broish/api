4b6a01aa19e723c29a8486107c12d6ae1bbcad64
Haralan Dobrev <harry@hkdobrev.com>
2021-07-04 18:23:58 +0300

    Script to bulk delete Firebase users

---
+++ scripts/delete-firebase-users.js
@@ -0,0 +1,38 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../firebase.json');
+
+require('dotenv').config();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://ti-broish.firebaseio.com',
+});
+
+function deleteUsers() {
+  return new Promise((resolve, reject) => {
+    admin
+      .auth()
+      .deleteUsers(process.argv[2].explode(','))
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+async function runScript() {
+  try {
+    // Start listing users from the beginning, 1000 at a time.
+    const deleteUsersResult = await deleteUsers();
+
+    console.log(`Successfully deleted ${deleteUsersResult.successCount} users`);
+    console.warn(`Failed to delete ${deleteUsersResult.failureCount} users`);
+    deleteUsersResult.errors.forEach((err) => {
+      console.error(err.error.toJSON());
+    });
+    process.exit(deleteUsersResult.failureCount > 0 ? 2 : 0);
+  } catch (error) {
+    console.error('Error deleting users:');
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runScript();
