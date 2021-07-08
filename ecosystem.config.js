34030be55dad85963aea95b675fa840c827e6579
Haralan Dobrev <harry@hkdobrev.com>
2021-07-08 19:32:37 +0300

    Add staging deployment to pm2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,6 +18,24 @@ module.exports = {
       out_file: 'var/log/out.log',
       log_file: 'var/log/combined.log',
     },
+    {
+      name: 'ti-broish-api-staging',
+      script: 'dist/main.js',
+
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      watch: true,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      env_staging: {
+        NODE_ENV: 'production', // This is intentional. We do not have a staging environment inside the app.
+      },
+      instances: 2,
+      error_file: 'var/log/err.log',
+      out_file: 'var/log/out.log',
+      log_file: 'var/log/combined.log',
+    },
   ],
 
   deploy: {
@@ -34,6 +52,20 @@ module.exports = {
         ' && cp $PWD/../shared/.env $PWD/../shared/firebase.json $PWD/' +
         ' && NODE_ENV=production npm run migration:run' +
         ' && pm2 startOrReload ecosystem.config.js --env production',
+    },
+    'ti-broish-api-staging': {
+      user: 'deploy',
+      host: ['ti-broish-api'],
+      ref: 'origin/main',
+      repo: 'https://github.com/ti-broish/api.git',
+      path: '/var/www/ti-broish-api-staging',
+      'post-setup': 'ls -la',
+      'post-deploy':
+        'npm ci --only=production --ignore-scripts' +
+        ' && npm run build' +
+        ' && cp $PWD/../shared/.env $PWD/../shared/firebase.json $PWD/' +
+        ' && NODE_ENV=production npm run migration:run' +
+        ' && pm2 startOrReload ecosystem.config.js --env staging',
     },
   },
 };
