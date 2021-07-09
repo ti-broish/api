cac0c79f15ef853fec8fb1bea423cd5b0fa88d46
Haralan Dobrev <harry@hkdobrev.com>
2021-07-09 05:34:06 +0300

    Complete remove not null constraint from stream chunk

---
+++ src/migrations/1625797593531-StreamChunksStartTimestampNotNull.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class StreamChunksStartTimestampNotNull1625797593531
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        ALTER TABLE stream_chunks ALTER COLUMN start_timestamp DROP NOT NULL;
+      `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        ALTER TABLE stream_chunks ALTER COLUMN start_timestamp SET NOT NULL;
+      `);
+  }
+}
