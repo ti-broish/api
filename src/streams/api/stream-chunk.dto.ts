aa953fa464c8fdecaa0c40dae7a6b1fbb7b291d7
Haralan Dobrev <harry@hkdobrev.com>
2021-07-11 20:22:37 +0300

    Expose chunk fields

--- src/streams/api/stream-chunk.dto.ts
+++ src/streams/api/stream-chunk.dto.ts
@@ -2,15 +2,15 @@ import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
 export class StreamChunkDto {
-  @Expose({ groups: ['stream.watch'] })
+  @Expose({ groups: ['stream.watch', 'stream.feed'] })
   id: string;
 
-  @Expose({ groups: ['stream.watch'] })
+  @Expose({ groups: ['stream.watch', 'stream.feed'] })
   url?: string;
 
-  @Expose({ groups: ['stream.watch'] })
+  @Expose({ groups: ['stream.watch', 'stream.feed'] })
   startTime: Date;
 
-  @Expose({ groups: ['stream.watch'] })
+  @Expose({ groups: ['stream.watch', 'stream.feed'] })
   endTime?: Date;
 }
