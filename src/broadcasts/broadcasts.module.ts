18cd342ab0a068793fb6714b5e44aab6d65ed665
Haralan Dobrev <harry@hkdobrev.com>
2022-10-02 17:33:21 +0300

    Disable broadcast notifications

--- src/broadcasts/broadcasts.module.ts
+++ src/broadcasts/broadcasts.module.ts
@@ -4,12 +4,11 @@ import { UsersModule } from '../users/users.module';
 import { Broadcast } from '../broadcasts/entities/broadcast.entity';
 import { BroadcastsController } from './api/broadcasts.controller';
 import { BroadcastsRepository } from '../broadcasts/entities/broadcasts.repository';
-import { BroadcastsPublishTask } from './notifications/broadcasts-publish.task';
 import { CaslModule } from 'src/casl/casl.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Broadcast]), CaslModule, UsersModule],
-  providers: [BroadcastsRepository, BroadcastsPublishTask],
+  providers: [BroadcastsRepository],
   exports: [],
   controllers: [BroadcastsController],
 })
