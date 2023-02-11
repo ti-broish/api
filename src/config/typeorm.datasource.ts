55d3a52eca6ea98b2a56a2e44a06216443232b77
Haralan Dobrev <harry@hkdobrev.com>
2023-02-11 22:57:04 +0200

    Configure TypeORM datasource for migrations to be dynamic

--- src/config/typeorm.datasource.ts
+++ src/config/typeorm.datasource.ts
@@ -1,12 +1,23 @@
 import { DataSource } from 'typeorm'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const {
+  DATABASE_HOST,
+  DATABASE_NAME,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_PORT,
+} = process.env
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'ti-broish-db',
-  port: 5432,
-  username: 'postgres',
-  password: 'ti-broish',
-  database: 'ti_broish',
+  host: DATABASE_HOST,
+  port: parseInt(DATABASE_PORT, 10),
+  username: DATABASE_USERNAME,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
   logging: true,
   synchronize: false,
   migrations: ['dist/migrations/*.js'],
