95075704ffdc3710cb18e598a229027fdc1ce1ad
Haralan Dobrev <harry@hkdobrev.com>
2023-02-13 03:34:24 +0200

    Allow null author in violation update entity

--- src/violations/entities/violation-update.entity.ts
+++ src/violations/entities/violation-update.entity.ts
@@ -33,7 +33,7 @@ export class ViolationUpdate {
   violation: Violation
 
   @ManyToOne(() => User)
-  actor: User
+  actor: User | null
 
   @Column({ type: 'varchar' })
   type: ViolationUpdateType
