4aec91242ed1fc402564bed7ba2f769d42158b54
Haralan Dobrev <harry@hkdobrev.com>
2023-03-09 01:09:20 +0200

    Simplify crumb maker a bit

--- src/results/api/crumb-maker.service.ts
+++ src/results/api/crumb-maker.service.ts
@@ -1,38 +1,53 @@
 import { Inject } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { Municipality, Section } from 'src/sections/entities'
+import {
+  CityRegion,
+  Country,
+  ElectionRegion,
+  Municipality,
+  Section,
+} from 'src/sections/entities'
+import { MUNICIPALITIES_HIDDEN } from 'src/sections/sections.constants'
 import { mapToType, NodeType } from './results.controller'
+
+export interface Crumb {
+  segment: string
+  name: string
+  type: NodeType
+}
 
 export class CrumbMaker {
   constructor(@Inject(ConfigService) private readonly config: ConfigService) {}
 
-  makeCrumbs(items: any[]) {
-    const segments = []
+  makeCrumbs(
+    items: (ElectionRegion | Municipality | Country | CityRegion | Section)[],
+  ) {
+    const segments: string[] = []
     return items
       .filter((x) => !!x)
       .reduce(
-        (crumbs: Record<string, string>[], item) => {
+        (crumbs: Crumb[], item): Crumb[] => {
           segments.push(item.code)
-          if (item instanceof Municipality && item.isMunicipalityHidden()) {
+          if (
+            item instanceof Municipality &&
+            MUNICIPALITIES_HIDDEN.includes(segments.join(''))
+          ) {
             return crumbs
           }
 
           crumbs.push({
-            segment:
-              item instanceof Section
-                ? item.id
-                : segments.reduce((acc, x) => `${acc}${x}`, ''),
-            name: item.name,
+            segment: item instanceof Section ? item.id : segments.join(''),
+            name: item instanceof Section ? item.code : item.name,
             type: mapToType(item),
-          })
+          } as Crumb)
           return crumbs
         },
         [
           {
             segment: '',
-            name: this.config.get('ELECTION_CAMPAIGN_NAME'),
+            name: this.config.get<string>('ELECTION_CAMPAIGN_NAME'),
             type: NodeType.ELECTION,
-          },
+          } as Crumb,
         ],
       )
   }
