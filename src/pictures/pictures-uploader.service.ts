2e34c9b8ddd603a790e9f940f7ae8049ad45b534
Haralan Dobrev <harry@hkdobrev.com>
2023-04-02 02:20:41 +0300

    Correct file deletion in generic file uploader and to delete filepath, not CDN URL

--- src/pictures/pictures-uploader.service.ts
+++ src/pictures/pictures-uploader.service.ts
@@ -2,7 +2,6 @@ import { Inject, Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { FilesUploader } from '../files'
 import { Picture } from './entities/picture.entity'
-import * as fs from 'fs'
 
 @Injectable()
 export class PicturesUploader {
@@ -11,23 +10,12 @@ export class PicturesUploader {
     @Inject(FilesUploader) private readonly uploader: FilesUploader,
   ) {}
 
-  deletePictureFromLocal(path: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      fs.unlink(path, (err) => {
-        if (err) {
-          reject(err)
-        }
-        resolve('Deleted file successfully!')
-      })
-    })
-  }
-
   async upload(base64EncodedFile: string): Promise<Picture> {
     const picture = new Picture()
     picture.path = await this.uploader.uploadFileToCdn(
       await this.uploader.base64ToLocalFilePath(base64EncodedFile),
     )
-    await this.deletePictureFromLocal(picture.path)
+
     return picture
   }
 }
